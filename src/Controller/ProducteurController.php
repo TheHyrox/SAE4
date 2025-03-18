ff48d9aaa6d16d238725c0c0679236bf9be64364
Matthis <matthisgastesoyer@gmail.com>
2025-03-18 08:56:11 +0100

    Created ProducteurController & template

---
+++ src/Controller/ProducteurController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\PRODUCTEUR;
+use App\Entity\UTILISATEUR;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+final class ProducteurController extends AbstractController
+{
+    #[Route('/producteur/{id}', name: 'app_producteur')]
+    public function show(int $id, EntityManagerInterface $entityManager): Response
+    {
+        $queryBuilder = $entityManager->createQueryBuilder();
+        $queryBuilder
+            ->select('u', 'p')
+            ->from(UTILISATEUR::class, 'u')
+            ->innerJoin(PRODUCTEUR::class, 'p', 'WITH', 'u.id = p.Id_Uti')
+            ->where('p.id = :id')
+            ->setParameter('id', $id);
+
+        $producteur = $queryBuilder->getQuery()->getResult();
+
+        return $this->render('producteur/show.html.twig', [
+            dd($producteur),
+            'producteur' => $producteur,
+        ]);
+    }
+}
