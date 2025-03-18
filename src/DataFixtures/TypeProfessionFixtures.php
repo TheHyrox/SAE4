28c587b5bfb306b32835f29e1a51bddbedd7dc11
Matthis <matthisgastesoyer@gmail.com>
2025-03-18 15:41:50 +0100

    Update typeProfession fixtures to add ids

--- src/DataFixtures/TypeProfessionFixtures.php
+++ src/DataFixtures/TypeProfessionFixtures.php
@@ -1,13 +1,15 @@
 <?php
-
 namespace App\DataFixtures;
 
 use App\Entity\TypeProfession;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use Doctrine\ORM\Id\AssignedGenerator;
+use Doctrine\ORM\Mapping\ClassMetadata;
 
 class TypeProfessionFixtures extends Fixture
 {
+    // Constants
     public const PROFESSION_AGRICULTEUR = 'profession_agriculteur';
     public const PROFESSION_VIGNERON = 'profession_vigneron';
     public const PROFESSION_MARAICHER = 'profession_maraicher';
@@ -18,18 +20,24 @@ class TypeProfessionFixtures extends Fixture
 
     public function load(ObjectManager $manager): void
     {
+        // Configure to use assigned IDs instead of auto-increment
+        $metadata = $manager->getClassMetadata(TypeProfession::class);
+        $metadata->setIdGeneratorType(ClassMetadata::GENERATOR_TYPE_NONE);
+        $metadata->setIdGenerator(new AssignedGenerator());
+
         $professions = [
-            [self::PROFESSION_AGRICULTEUR, 'Agriculteur'],
-            [self::PROFESSION_VIGNERON, 'Vigneron'],
-            [self::PROFESSION_MARAICHER, 'Maraîcher'],
-            [self::PROFESSION_APICULTEUR, 'Apiculteur'],
-            [self::PROFESSION_ELEVEUR_VOLAILLE, 'Éleveur de volaille'],
-            [self::PROFESSION_VITICULTEUR, 'Viticulteur'],
-            [self::PROFESSION_PEPINIERISTE, 'Pépiniériste'],
+            [self::PROFESSION_AGRICULTEUR, 'Agriculteur', 1],
+            [self::PROFESSION_VIGNERON, 'Vigneron', 2],
+            [self::PROFESSION_MARAICHER, 'Maraîcher', 3],
+            [self::PROFESSION_APICULTEUR, 'Apiculteur', 4],
+            [self::PROFESSION_ELEVEUR_VOLAILLE, 'Éleveur de volaille', 5],
+            [self::PROFESSION_VITICULTEUR, 'Viticulteur', 6],
+            [self::PROFESSION_PEPINIERISTE, 'Pépiniériste', 7],
         ];
 
-        foreach ($professions as [$reference, $name]) {
+        foreach ($professions as [$reference, $name, $id]) {
             $profession = new TypeProfession();
+            $profession->setId($id); // You'll need to add this method
             $profession->setName($name);
 
             $manager->persist($profession);
